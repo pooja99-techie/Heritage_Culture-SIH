3846b0796fc5f00aa41c398f01adb4821ef6b829
Pooja Sharma <dr.pooja11999@gmail.com>
2024-10-15 20:47:12 +0530

    Create sign up API route

--- Heritage and Culture Project/api/index.js
+++ Heritage and Culture Project/api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -14,9 +15,12 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
+app.use(express.json()); 
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
     }
 );
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
